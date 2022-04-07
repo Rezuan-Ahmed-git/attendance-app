d1c15934e83df0911383ea7620b542f40ba15c00
Rezuan Ahmed <zikzack06@gmail.com>
2022-04-07 14:26:17 +0600

    feat: refactor attendance app>>>

---
+++ controller/auth.js
@@ -0,0 +1,40 @@
+const { registerService, loginService } = require("../service/auth");
+
+/* Request Body Sources:
+      -req Body
+      -req param
+      -req query
+      -req header
+      -req cookies
+    */
+
+const registerController = async (req, res, next) => {
+  const { name, email, password } = req.body;
+
+  //   validation
+  if (!name || !email || !password) {
+    return res.status(400).json({ message: "Invalid data" });
+  }
+
+  try {
+    const user = await registerService({ name, email, password });
+    return res.status(201).json({ message: "User Created Successfully", user });
+  } catch (err) {
+    next(err);
+  }
+};
+
+const loginController = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const token = await loginService({ email, password });
+    return res.status(200).json({ message: "Login Successful", token });
+  } catch (e) {
+    next(e);
+  }
+};
+
+module.exports = {
+  registerController,
+  loginController,
+};
--- server.js
+++ server.js
@@ -1,65 +1,21 @@
 const express = require("express");
 const connectDB = require("./db");
-const User = require("./models/User");
-const bcrypt = require("bcryptjs");
+const authenticate = require("./middleware/authenticate");
+const routes = require("./routes/index");
 const app = express();
 
 app.use(express.json());
+app.use(routes);
 
-app.post("/register", async (req, res, next) => {
-  /* Request Body Sources:
-    -req Body
-    -req param
-    -req query
-    -req header
-    -req cookies
-  */
-  const { name, email, password } = req.body;
-  if (!name || !email || !password) {
-    return res.status(400).json({ message: "Invalid data" });
-  }
-
-  try {
-    let user = await User.findOne({ email });
-    if (user) {
-      return res.status(400).json({ message: "User already exist" });
-    }
-
-    user = new User({ name, email, password });
-
-    const salt = await bcrypt.genSalt(10);
-    const hash = await bcrypt.hash(password, salt);
-    user.password = hash;
-
-    await user.save();
-    return res.status(201).json({ message: "User Created Successfully", user });
-  } catch (err) {
-    next(err);
-  }
+app.get("/private", authenticate, async (req, res) => {
+  console.log("I am User", req.user);
+  return res.status(200).json({ message: "I am a Private router" });
+});
+app.get("/public", (_req, res) => {
+  return res.status(200).json({ message: "I am a public router" });
 });
 
-app.post("/login", async (req, res, next) => {
-  const { email, password } = req.body;
-  try {
-    const user = await User.findOne({ email });
-
-    if (!user) {
-      return res.status(400).json({ message: "Invalid Credential" });
-    }
-
-    const isMatch = await bcrypt.compare(password, user.password);
-
-    if (!isMatch) {
-      return res.status(400).json({ message: "Invalid Credential" });
-    }
-    delete user._doc.password;
-    return res.status(200).json({ message: "Login Successful", user });
-  } catch (e) {
-    next(e);
-  }
-});
-
-app.get("/", (req, res) => {
+app.get("/", (_req, res) => {
   const obj = {
     name: "Rezuan",
     email: "rezuan@gmail.com",
@@ -67,9 +23,11 @@ app.get("/", (req, res) => {
   res.json(obj);
 });
 
-app.use((err, req, res, next) => {
+app.use((err, _req, res, _next) => {
   console.log(err);
-  res.status(500).json({ message: "Server Error Occurred" });
+  const message = err.message ? err.message : "Server Error Occurred";
+  const status = err.status ? err.status : 500;
+  res.status(status).json({ message });
 });
 
 connectDB("mongodb://localhost:27017/attendance-app")
